79b6d6a5e0e563a3dc5ec35ad3602edd1a7681ca
Akira-Ito0109 <syobonnito@gmail.com>
2021-02-08 14:27:00 +0900

    10終了

---
+++ Assets/scripts/TankHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour
+{
+
+    public int tankHP;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="EnemyShell")
+        {
+            tankHP -= 1;
+
+            Destroy(other.gameObject);
+
+            if(tankHP<=0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
