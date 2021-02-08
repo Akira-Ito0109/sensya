a2e5e80ad1867e09c9d8e4f841d7c5728abf924d
Akira-Ito0109 <syobonnito@gmail.com>
2021-02-08 14:48:59 +0900

    12終了

---
+++ Assets/scripts/Chasecamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chasecamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target!=null)
+        {
+            transform.position = target.transform.position + offset;
+        }
+    }
+}
